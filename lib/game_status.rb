92952f3db926292160f9bb3a0b3266748918619c
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:21:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,14 +6,14 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
-  def won?(array)
-    WIN_COMBINATIONS.each do |winner|
-      if array[winner[0]] == array[winner[1]] &&
-         array[winner[1]] == array[winner[2]] &&
-         position_taken?(array, winner[0])
-         puts "somebody won!"
-         return true
-         return winner
-      end
-    end
+def won?(array)
+WIN_COMBINATIONS.each do |winner_set|
+  if array[winner_set[0]] == array[winner_set[1]] &&
+     array[winner_set[1]] == array[winner_set[2]] &&
+     position_taken?(array, winner_set[0])
+     puts "somebody won!"
+     return true 
+     return winner_set
   end
+end
+end
