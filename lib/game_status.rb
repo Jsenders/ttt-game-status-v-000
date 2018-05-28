093b45dd222e2f45de1a5f86cb51556bbae5157a
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:04:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
-def won?
+def won?(board)
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 top_row_win = [0,1,2]
 if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
