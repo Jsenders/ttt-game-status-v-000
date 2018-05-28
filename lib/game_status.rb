df57a705dedf24fc0dedcd1a691002916e94d528
Joseph Senders <jasenders@gmail.com>
2018-05-28 01:34:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,12 +8,10 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5
 
 def won?(board)
 winner = []
-empty_board = board.all? {|x| x == " "}
+empty_board = board.all? 
 WIN_COMBINATIONS.each do |array|
-    if empty_board || full?(board) 
-      return false
-    elsif array.all? { |value| board[value] =="X" } || array.all? { |value| board[value] =="O" }
-      winner = array 
+    if array.all? { |value| board[value] =="X" } || array.all? { |value| board[value] =="O" }
+      winner = array
     end
   end
 end
