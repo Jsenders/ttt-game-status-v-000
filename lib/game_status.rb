856e078138b4f4fea873ff80e6e334ef7a0b65db
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:04:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,5 +9,6 @@ def won?(board)
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 top_row_win = [0,1,2]
 if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  "X won in the top row"
+  puts "X won in the top row"
+end
 end
