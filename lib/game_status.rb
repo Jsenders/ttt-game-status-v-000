0f7b2f93c0513678fe1b57b77ce25cdeb3dd6cc4
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:21:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,13 +5,15 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
-def won?(board)
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-top_row_win = [0,1,2]
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  puts "X won in the top row"
-end
-end
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-won?(board)
+  def won?(array)
+    WIN_COMBINATIONS.each do |winner|
+      if array[winner[0]] == array[winner[1]] &&
+         array[winner[1]] == array[winner[2]] &&
+         position_taken?(array, winner[0])
+         puts "somebody won!"
+         return true 
+         return winner
+      end
+    end
+  end
