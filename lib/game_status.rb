af66d1b194f1a1f4ec9038637179461d4e9b8017
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:12:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,17 +6,17 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 def won?(board)
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-top_row_win = [0,1,2]
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+board = [["X", "X", "X", " ", " ", " ", " ", " ", " "],[ " ", " ", " ", "X", "X", "X", " ", " ", " "],[ " ", " ", " ", "X", "X", "X", " ", " ", " "]]
+top_row_win = board[0]
+if board[top_row_win] == "X" && board[top_row_win] == "X" && board[top_row_win] == "X"
   puts "X won in the top row"
 board = [ " ", " ", " ", "X", "X", "X", " ", " ", " "]
-middle_row_win = [3,4,5]
-if board[middle_row_win[0]] == "X" && board[middle_row_win[1]] == "X" && board[middle_row_win[2]] == "X"
+middle_row_win = board[1]
+if board[middle_row_win] == "X" && board[middle_row_win] == "X" && board[middle_row_win] == "X"
   puts "X won in the middle row"
 board = [ " ", " ", " ", "X", "X", "X", " ", " ", " "]
-bottom_row_win = [6,7,8]
-if board[bottom_row_win[0]] == "X" && board[bottom_row_win[1]] == "X" && board[bottom_row_win[2]] == "X"
+bottom_row_win = board[2]
+if board[bottom_row_win] == "X" && board[bottom_row_win] == "X" && board[bottom_row_win] == "X"
   puts "X won in the bottom row"
       end
     end
