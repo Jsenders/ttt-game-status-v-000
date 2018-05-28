0c7ef12b98e3fa9bb14333debc5ea2a64b69b520
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:26:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,12 +7,14 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
 def won?(board)
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-top_row_win = [0,1,2]
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  puts "X won in the top row"
+winner = []
+empty_board = board.all? {|x| x == " "}
+WIN_COMBINATIONS.each do |sub_array|
+    if empty_board || full?(board)
+      return false
+    elsif sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
+      winner = sub_array
+    end
+  end
+  winner
 end
-end
-
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-won?(board)
