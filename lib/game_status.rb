6dc95b7eb5f119fa4e5f4256eb422c7eadf7f5e9
Joseph Senders <jasenders@gmail.com>
2018-05-28 00:21:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,7 +12,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5
          array[winner[1]] == array[winner[2]] &&
          position_taken?(array, winner[0])
          puts "somebody won!"
-         return true 
+         return true
          return winner
       end
     end
